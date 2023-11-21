b4375d1b1c1a8b556ab1d63cb3ee845870af3967
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-11-21 12:45:35 -0500

    Revert "try file stuff"
    This reverts commit d9168343ef53f45899880b8e57659c12653a745a.

--- app/src/main/java/com/example/anikash/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/anikash/ui/dashboard/DashboardFragment.kt
@@ -1,100 +1,46 @@
-import android.content.Context
-import android.content.SharedPreferences
+package com.example.anikash.ui.dashboard
+
 import android.os.Bundle
-import android.preference.PreferenceManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.anikash.databinding.FragmentDashboardBinding
-import com.example.anikash.ui.dashboard.DashboardViewModel
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.InputStreamReader
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
 
-    // Key for storing and retrieving the text in SharedPreferences
-    private val FILE_NAME = "stored_text.txt"
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+
+
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textView
-
-        // Explicitly specify the type when getting the ViewModel
-        val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
 
-        binding.submitButton.setOnClickListener {
-            // Store the text in SharedPreferences
-
-            val textToStore = binding.textField.text.toString()
-            val applicationContext: Context = requireActivity().applicationContext
-
-            saveTextToFile(applicationContext, textToStore)
-            binding.submitButton.text = getStoredText(applicationContext)
-
-            Toast.makeText(applicationContext, "Wrote to file", Toast.LENGTH_SHORT).show()
+        binding.submitButton.setOnClickListener{
+            binding.submitButton.text = binding.textField.text
         }
-
-        val applicationContext: Context = requireActivity().applicationContext
-        binding.textView3.text = getStoredText(applicationContext)
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    // Crete the save to file function
-    fun saveTextToFile(context: Context, text: String) {
-        try {
-            val fileOutputStream: FileOutputStream = File(context.filesDir, FILE_NAME).outputStream()
-            fileOutputStream.write(text.toByteArray())
-            fileOutputStream.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    // create the get stored text function with
-    fun getStoredText(context: Context): String {
-        try {
-            val fileInputStream: FileInputStream = File(context.filesDir, FILE_NAME).inputStream()
-            val inputStreamReader = InputStreamReader(fileInputStream)
-            val bufferedReader = BufferedReader(inputStreamReader)
-            val stringBuilder = StringBuilder()
-            var text: String? = null
-
-            while ({ text = bufferedReader.readLine(); text }() != null) {
-                stringBuilder.append(text)
-            }
-
-            fileInputStream.close()
-            return stringBuilder.toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return ""
     }
 }
--- app/src/main/java/com/example/anikash/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/anikash/ui/notifications/NotificationsFragment.kt
@@ -1,8 +1,5 @@
 package com.example.anikash.ui.notifications
 
-import DashboardFragment
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,14 +8,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.anikash.databinding.FragmentNotificationsBinding
-import java.io.BufferedReader
-import java.io.FileInputStream
-import java.io.InputStreamReader
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-    private val dashboardFragment = DashboardFragment()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -39,44 +32,11 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-
-        // Retrieve stored text from DashboardFragment
-        val applicationContext: Context = requireActivity().applicationContext
-
-        val storedText = dashboardFragment.getStoredText(applicationContext)
-        System.out.println(storedText)
-
-        // Now, you can use the storedText as needed
-        // For example, set it to a TextView
-        binding.textView4.text = storedText
-
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    // Sample method to retrieve stored text from SharedPreferences
-    fun getStoredText(): String {
-        try {
-            val fileInputStream: FileInputStream = requireContext().openFileInput("stored_text.txt")
-            val inputStreamReader = InputStreamReader(fileInputStream)
-            val bufferedReader = BufferedReader(inputStreamReader)
-            val stringBuilder = StringBuilder()
-            var text: String? = null
-
-            while ({ text = bufferedReader.readLine(); text }() != null) {
-                stringBuilder.append(text)
-            }
-
-            fileInputStream.close()
-            return stringBuilder.toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return ""
     }
 }
