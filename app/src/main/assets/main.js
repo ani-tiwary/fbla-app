0f989dcefd3194079cadc488f0f094d467f24dc3
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-01-19 11:55:11 -0500

    add people

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -22,7 +22,7 @@ function sendAndroidMessage() {
 	`${document.getElementById("shortDescription").innerText} `);
 }
 
-var data = '[{"Name":"Martin Luther King Jr.","Organization":"Civil Rights Movement","Position":"Leader","Education":"Boston University School of Theology","Skills":"Speaking, Leadership, Activism","Honors":"Noble Prize, NAACP Spingarn Medal, Presidential Medal of Freedom","References":"Malcolm X, Rosa Parks, John F. Kennedy","EmploymentStatus":"N/A","imageURL":"https://upload.wikimedia.org/wikipedia/commons/0/05/Martin_Luther_King%2C_Jr..jpg"}, {"Name":"Rosa Parks","Organization":"Civil Rights Movement","Position":"Activist","Education":"Alabama State Teachers College","Skills":"Activism, Leadership","Honors":"Congressional Gold Medal, Presidential Medal of Freedom","References":"Martin Luther King Jr., Malcolm X, John F. Kennedy","EmploymentStatus":"N/A","imageURL":"https://upload.wikimedia.org/wikipedia/commons/c/c4/Rosaparks.jpg"}, {"Name":"Elon Musk","Organization":"Tesla","Position":"CEO","Education":"University of Pennsylvania","Skills":"Leadership, Engineering","Honors":"Very Rich Man","References":"Jeff Bezos, Bill Gates, Steve Jobs","EmploymentStatus":"Employed","imageURL":"https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg"}]'
+var data = '[{"Name":"Martin Luther King Jr.","Organization":"Civil Rights Movement","Position":"Leader","Education":"Boston University School of Theology","Skills":"Speaking, Leadership, Activism","Honors":"Noble Prize, NAACP Spingarn Medal, Presidential Medal of Freedom","References":"Malcolm X, Rosa Parks, John F. Kennedy","EmploymentStatus":"Unemployed","imageURL":"https://upload.wikimedia.org/wikipedia/commons/0/05/Martin_Luther_King%2C_Jr..jpg"}, {"Name":"Rosa Parks","Organization":"Civil Rights Movement","Position":"Activist","Education":"Alabama State Teachers College","Skills":"Activism, Leadership","Honors":"Congressional Gold Medal, Presidential Medal of Freedom","References":"Martin Luther King Jr., Malcolm X, John F. Kennedy","EmploymentStatus":"N/A","imageURL":"https://upload.wikimedia.org/wikipedia/commons/c/c4/Rosaparks.jpg"}, {"Name":"Elon Musk","Organization":"Tesla","Position":"CEO","Education":"University of Pennsylvania","Skills":"Leadership, Engineering","Honors":"Very Rich Man","References":"Jeff Bezos, Bill Gates, Steve Jobs","EmploymentStatus":"Employed","imageURL":"https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg"}]'
 
 function getData() {
     // This JSON files is hosted over the web
@@ -59,27 +59,28 @@ function insertRow(data, i) {
     var textColor = "text-dark";
     if (data.EmploymentStatus == "Employed") {
         color = "green_gradient";
-        textColor = "text-white";
     } else if (data.EmploymentStatus == "Unemployed") {
         color = "red_gradient";
-        textColor = "text-white";
     }
     
     div.innerHTML += `
-    <div class="card ${color} ${textColor} mb-3 mr-2 ml-2 mt-1" onclick="openPortfolio(${i})" style="cursor: pointer">
-      <div class="card-header">
-        <h2 class="card-title ${textColor}">${data.Name}</h2>
+    <div class="card ${color} ${textColor} mb-3 mr-2 ml-2 mt-1 pt-3" onclick="openPortfolio(${i})" style="cursor: pointer">
+    <div class="row no-gutters">
+      <div class="col-md-4 text-center">
+        <img src="${data.imageURL}" class="card-img text-center" alt="${data.Name}" style="border-radius: 5%; width: 50%;">
       </div>
-      <div class="card-body">
-        <p class="card-text"><i class="fas fa-building"></i> ${data.Organization}</p>
-        <p class="card-text"><i class="fas fa-briefcase"></i> ${data.Position}</p>
-        <p class="card-text"><i class="fas fa-university"></i> ${data.Education}</p>
-      </div>
-      <div class="card-body">
-        <p class="card-text"><i class="fas fa-cogs"></i> ${data.Skills}</p>
-        <p class="card-text"><i class="fas fa-medal"></i> ${data.Honors}</p>
-        <p class="card-text"><i class="fas fa-users"></i> ${data.References}</p>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h2 class="card-title ${textColor}">${data.Name}</h2>
+          <p class="card-text"><i class="fas fa-building"></i> ${data.Organization}</p>
+          <p class="card-text"><i class="fas fa-briefcase"></i> ${data.Position}</p>
+          <p class="card-text"><i class="fas fa-university"></i> ${data.Education}</p>
+          <p class="card-text"><i class="fas fa-cogs"></i> ${data.Skills}</p>
+          <p class="card-text"><i class="fas fa-medal"></i> ${data.Honors}</p>
+          <p class="card-text"><i class="fas fa-users"></i> ${data.References}</p>
+        </div>
       </div>
     </div>
+  </div>
     `;
 }
